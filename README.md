2ad65f645b783a4c4a0ff1aea0ea539c49787a9a
zoolpher <tmdoom48@gmail.com>
2026-09-07 13:23:28 +0530

    Initialize bnb-lens

---
+++ README.md
@@ -0,0 +1,1 @@
+Here we will capture live data from binance public websocket
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Starting bnb-lens data capture..." << std::endl;
+    
+    // We will eventually add command-line argument parsing here
+    // to read things like "--venue spot" and "--symbols BTCUSDT"
+    
+    return 0;
+}
